cc84f1d362014d0c21e12b271f31103bf3c7d2ab
naoki000 <niwata.521@gmail.com>
2018-07-21 20:13:29 +0900

    test

--- raspi_to_googlehome_notifier/src/main.js
+++ raspi_to_googlehome_notifier/src/main.js
@@ -3,7 +3,7 @@ const language = 'ja';
 const ipaddr = "192.168.43.131"
 googlehome.device("Google-Home", language);
 googlehome.ip(ipaddr);
-googlehome.speed(2);
+googlehome.speed(0.5);
 googlehome.notify("こんにちは", function(res) {
     console.log(res);
 });
