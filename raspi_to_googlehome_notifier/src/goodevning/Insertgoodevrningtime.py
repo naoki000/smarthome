c28c43b7670f3db4aa68231e17f3cbb7215bb197
naoki000 <niwata.521@gmail.com>
2018-07-23 00:24:49 +0900

    t

--- raspi_to_googlehome_notifier/src/goodevning/Insertgoodevrningtime.py
+++ raspi_to_googlehome_notifier/src/goodevning/Insertgoodevrningtime.py
@@ -4,8 +4,7 @@ import datetime
 import xml.etree.ElementTree as ET
 import requests
 import mysql.connector
-today = datetime.datetime.today().strftime("%Y/%m/%d")
-
+today = datetime.datetime.now()
 
 
 # Get rainfallchance of North Yokohama
@@ -26,12 +25,12 @@ def sqlinsert():
     db = cnt.cursor(buffered=True)
 
     # SQLクエリ実行（データ追加）
-    sql = 'INSERT INTO Badtime VALUES ('+ '\''+today+'\');'
+    sql = 'INSERT INTO Bedtime VALUES ('+ '\''+str(today)+'\');'
  #   print (sql)    
     db.execute(sql)
-#    db.execute("select * from Badtime;")
-#    for row in db.fetchall():
-  #      print(row)
+    db.execute("select * from Bedtime;")
+    for row in db.fetchall():
+        print(row)
     db.close()
     cnt.commit() #反映
     # MySQL切断
